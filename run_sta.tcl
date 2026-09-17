5687dff81219622c3592c1f141ca673f1f6f841c
MinhND <d.minh71205@gmail.com>
2026-09-17 17:39:26 +0700

    run_sta_10ns.tcl

--- run_sta.tcl
+++ run_sta.tcl
@@ -3,7 +3,7 @@ set LIB_PATH "/input/sky130_fd_sc_hd__tt_025C_1v80.lib"
 read_liberty $LIB_PATH
 read_verilog /input/seq_adder_netlist.v
 link_design seq_adder
-read_sdc /input/seq_adder.sdc
+read_sdc /input/seq_adder_10ns.sdc
 
 report_checks -path_delay max
 report_checks -path_delay min
