32d886d3a1a21d84f3645be335d4ee73c2f3565a
MinhND <d.minh71205@gmail.com>
2026-09-17 17:33:04 +0700

    run_sta_2ns.tcl

---
+++ results/run_sta_2ns.tcl
@@ -0,0 +1,11 @@
+set LIB_PATH "/input/sky130_fd_sc_hd__tt_025C_1v80.lib"
+
+read_liberty $LIB_PATH
+read_verilog /input/seq_adder_netlist.v
+link_design seq_adder
+read_sdc /input/seq_adder_2ns.sdc
+
+report_checks -path_delay max
+report_checks -path_delay min
+report_tns
+report_wns
